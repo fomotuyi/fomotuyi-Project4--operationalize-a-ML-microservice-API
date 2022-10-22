392fe3d8262410faf87aa22e91ab08f85a0d05e9
Fiyinfoluwa <fiyinomotuyi@gmail.com>
2022-10-22 10:50:52 +0100

    added comment

--- resize.sh
+++ resize.sh
@@ -3,7 +3,7 @@
 # Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
 SIZE=${1:-20}
 
-# Get the ID of the environment host Amazon EC2 instance.
+# Get the ID of the environment host Amazon EC2 instance kk.
 INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
